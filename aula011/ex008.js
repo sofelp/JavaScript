2d799b79ae22c55bfac4ef1fe8a1326991d68265
sofelp <souto7960@gmail.com>
2024-09-25 17:55:05 -0300

    a

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 13
+console.log(`Sua Velocidade é de ${vel} Km/h`)
+if (vel > 12) {
+    console.log('Você Ultrapassou o Limite de Velocidade!!, você está Multado.')
+}
+console.log('Diriga com Segurança, Ultulize os Cintos')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Franch'
+if (país == 'Brasil') {
+    console.log('Você é Brasileiro')
+}
+else {
+    console.log('Você é Gringo')
+}
