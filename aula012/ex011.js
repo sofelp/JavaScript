c0fc44128d8529e2683a389affb01c448b95158c
sofelp <souto7960@gmail.com>
2024-09-28 02:42:06 -0300

    hora

--- aula012/ex011.js
+++ aula012/ex011.js
@@ -1,10 +1,16 @@
-var idade = 12
-if (idade >= 18 && idade <65) {
+var voto = 64
+
+if ((voto >= 18) && voto <65){
     console.log('Você Deve Votar')
-}  
+}
+
 else {
-    if ((idade >=16 && idade <18) || idade >=65) {
-        console.log('Voto Opcional')
+
+    if((voto >= 16) || voto >64 ) {
+        console.log('Seu Voto é Opicional')
     }
-    else console.log('você não pode Votar, infelizmente')
+
+    else {
+        console.log('você não pode votar')
+    }
 }
