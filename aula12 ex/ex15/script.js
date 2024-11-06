c495c71e56ec19e996f8521dff3126f8305ccb15
sofelp <souto7960@gmail.com>
2024-11-06 02:13:03 -0300

    a

--- aula12 ex/ex15/script.js
+++ aula12 ex/ex15/script.js
@@ -1,17 +1,19 @@
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var foto = window.documento.getElemnetByid('foto')
+function verificar() {
     var data = new Date()
-    var hora = data.getHours
-    msg.innerHTML = (`agora são ${hora} horas`)
-    if (hora >0 && <=12) {
-        msg.innerHTML = (`agora São ${hora} da manhã`)
-    }
-    else if (hora >12 && <=18) {
-        msg.innerHTML = (`Agora são ${hora} da Tarde`)
-    }
-
-    else {
-        msg.innerHTML = (`agora são ${hora} da Noite`)
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    if (fano.value.length == 0 || fano.value > ano) {
+        window.alert('Verifique se foi digitado corretamente e tente dnv')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value) 
+        res.innerHTML = `Idade calculada: ${idade}`
+        var gênero = ''
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+        } else if (fsex[1].checked) {
+            gênero = 'mulher'
+        }
     }
 }
