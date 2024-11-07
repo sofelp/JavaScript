a3b431960baa55b94bad4f363b0e514527aa2a1b
sofelp <souto7960@gmail.com>
2024-11-07 17:31:48 -0300

    a

--- aula12 ex/ex15/script.js
+++ aula12 ex/ex15/script.js
@@ -10,12 +10,38 @@ function verificar() {
         var idade = ano - Number(fano.value) 
         res.innerHTML = `Idade calculada: ${idade}`
         var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >=0 && idade < 21) {
+                img.setAttribute('src', 'homenjovem.png')
+            }
+            else if (idade < 50) {
+                //adulto
+                img.setAttribute('src', 'homen40.png')
+            }
+            else {
+                //Velho
+                img.setAttribute('src', 'homenvelho.png')
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >=0 && idade < 21) {
+                //Jovem
+                img.setAttribute('scr', 'mulhernova.png')
+            }
+           else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'mulher40.png')
+            }
+            else {
+                //Velha
+                img.setAttribute('src', 'mulhervelha.png')
+            }
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
