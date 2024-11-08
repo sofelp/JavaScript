77130611798491e05aee425a7c3577358b43e280
sofelp <souto7960@gmail.com>
2024-11-08 02:35:15 -0300

    v

--- aula 13/ex 016.js
+++ aula 13/ex 016.js
@@ -3,3 +3,6 @@ while (c <= 8) {
     console.log(`Passo ${c}`)
     c++
 }
+
+
+// Aprendendo While no qual consite no teste lógigo no começo
---
+++ aula 13/ex 17.js
@@ -0,0 +1,6 @@
+// Aprendendo o do... while com o teste lógico no final
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while ( c <= 6)
