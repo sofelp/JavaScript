fe511433d6ca8aa2d5d0137510257c12f4fb439a
sofelp <souto7960@gmail.com>
2024-11-08 02:43:09 -0300

    c

---
+++ aula 13/node_cd12c73965433.tmp.js
@@ -0,0 +1,13 @@
+// aprendendo a estrutura de Repetição com variavél de controle a chamada 'for'//
+
+/* código de referência:
+ var c = 1
+ while (c <=6) {
+  console.log(`Passo ${c}`)
+  c++
+ }
+*/
+
+for (var c = 1 ; c <= 10 ; c+2) {
+    console.log(c)
+}
