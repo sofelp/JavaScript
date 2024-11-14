bb5215a60b3850804eb0f3807ebc9a573dcbeacc
sofelp <souto7960@gmail.com>
2024-11-14 02:47:54 -0300

    fe

---
+++ aula014/ex01/script.js
@@ -0,0 +1,21 @@
+function consultar() {
+    var inicio = window.document.getElementById('inicio').value;
+    var fim = window.document.getElementById('fim').value;
+    var passo = window.document.getElementById('passo').value;
+    var resultado = window.document.getElementsByClassName('resultado')[0];
+
+    // Convertendo os valores para números
+    inicio = parseInt(inicio);
+    fim = parseInt(fim);
+    passo = parseInt(passo);
+
+    var contagem = ""; // Variável para armazenar a sequência da contagem
+
+    // Realizando a contagem
+    for (var i = inicio; i <= fim; i += passo) {
+        contagem += i + " "; // Concatenando cada número com um espaço
+    }
+
+    // Exibindo o resultado
+    resultado.innerText = `Contagem: ${contagem}`;
+}
